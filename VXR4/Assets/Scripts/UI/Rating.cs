77fa9fb03c5af9c85136de176d40626583a13400
darkblade <darkblade1078@gmail.com>
2025-12-08 15:54:25 -0600

    Prefabs

---
+++ VXR4/Assets/Scripts/UI/Rating.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rating : MonoBehaviour
+{
+    public void OnNumberPressed(string number)
+    {
+        Debug.Log(number); // Add shit later
+    }
+}
